b3ec317a0d1fc71d6764239c8153cea17171c535
HyungUk Ryu <hw0603@naver.com>
2023-01-10 10:12:07 +0900

    Modify 12100 2048 (Easy)

--- BOJ/BF/12100/hw0603/12100.py
+++ BOJ/BF/12100/hw0603/12100.py
@@ -1,89 +1,45 @@
 from itertools import product
 from collections import deque
 import sys
-import copy
 
-# 상, 하, 좌, 우
 dr = (-1, 1, 0, 0)
 dc = (0, 0, -1, 1)
 
 N = int(sys.stdin.readline())
 orig_board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-
-# 보드 출력 함수 (테스트용)
-def printboard(int: dir, board: list):
-    data = ("상", "하", "좌", "우")
-    print(f"<< {data[dir]} >>")
-    print(*board, sep="\n")
-    print()
-
 # 방향에 맞게 이동 후 board 값 변경
-def moveLR(dir: int):
+def move(dir: int):
     global board
-
-    for i, row in enumerate(board):
+    for i in range(N):
         line = deque()
-        add = line.append if dir == 2 else line.appendleft
+        add = line.append if not dir % 2 else line.appendleft  # Up, Left인 경우 append, Down, Right인 경우 appendleft
         buffer = 0
-        for j in (range(N) if dir == 2 else range(N-1, -1, -1)):
-            # 현재 칸에 숫자가 있다면
-            if (cur := board[i][j]):
-                # 버퍼에 있는 수와 같다면
-                if (buffer == cur):
-                    add(buffer * 2) # 버퍼 flush
-                    buffer = 0 # 버퍼를 비움
-                # 같지 않다면
-                else:
-                    # 버퍼에 저장된 수가 있다면 flush
-                    if (buffer):
-                        add(buffer)
-                    buffer = cur # 버퍼에 현재 수 저장
-        add(buffer)
-        if (dir == 2):
-            line = list(line) + [0] * (N - len(line))
-        else:
-            line = [0] * (N - len(line)) + list(line)
-        board[i] = line
-
-
-def moveUD(dir: int):
-    global board
-
-    for i, row in enumerate(board):
-        line = deque()
-        add = line.append if dir == 0 else line.appendleft
-        buffer = 0
-        for j in (range(N) if dir == 0 else range(N-1, -1, -1)):
-            # 현재 칸에 숫자가 있다면
-            if (cur := board[j][i]):
-                # 버퍼에 있는 수와 같다면
-                if (buffer == cur):
-                    add(buffer * 2)  # 버퍼 flush
+        for j in (range(N) if not dir % 2 else range(N-1, -1, -1)):  # 이동 방향에 따라 인덱스 증가 방향 설정
+            cur = board[i][j] if dir >= 2 else board[j][i]
+            if (cur):  # 현재 칸에 0이 아닌 숫자가 있다면
+                if (buffer == cur):  # 버퍼에 있는 수와 같다면
+                    add(buffer << 1)  # 버퍼 flush
                     buffer = 0  # 버퍼를 비움
-                # 같지 않다면
-                else:
-                    # 버퍼에 저장된 수가 있다면 flush
-                    if (buffer):
+                else:  # 같지 않다면
+                    if (buffer):  # 버퍼에 저장된 수가 있다면 flush
                         add(buffer)
                     buffer = cur  # 버퍼에 현재 수 저장
-        add(buffer)
-        if (dir == 0):
-            line = list(line) + [0] * (N - len(line))
-        else:
-            line = [0] * (N - len(line)) + list(line)
-        
-        # print(line)
+        if (buffer):
+            add(buffer)  # 마지막까지 버퍼에 남아있는 수 flush
+        for _ in range(N - len(line)):
+            add(0)  # 부족한 원소 수 만큼 0 채우기
 
-        for idx, item in enumerate(line):
-            board[idx][i] = item
-
+        if (dir < 2):  # Up, Down
+            for idx, item in enumerate(line):
+                board[idx][i] = item
+        else:  # Left, Right
+            board[i] = list(line)
 
 result = 0
 for dirlist in product(range(4), repeat=5):
-    board = copy.deepcopy(orig_board)
+    board = [row[:] for row in orig_board]  # 단순 2D List의 경우 copy.deepcopy()보다 훨씬 빠름
     for dir in dirlist:
-        move = moveLR if dir > 1 else moveUD
         move(dir)
     result = max(max(max(x) for x in board), result)
 
--- BOJ/BF/12100/hw0603/README.md
+++ BOJ/BF/12100/hw0603/README.md
@@ -1,10 +1,32 @@
 ## Info
-<a href="https://www.acmicpc.net/problem/12100">
-    12100 2048 (Easy)
-</a>
+[12100 2048 (Easy)](https://www.acmicpc.net/problem/12100)
 
 ## 💡 풀이 방법 요약
-
+[13460 구슬 탈출 2](https://www.acmicpc.net/problem/13460) 와 비슷한 방식으로 풀이한다.  
+전체적인 구조는 `itertools.product`를 순회하는 메인 반복분 안에서 모든 경우를 BF를 통해 확인하고, 반복 시 마다 최댓값을 업데이트하는 구조이다. 
+```
+result = 0
+for dirlist in product(("상", "하", "좌", "우"), repeat=5):
+    board = [row[:] for row in orig_board]
+    for dir in dirlist:
+        move(dir)
+    result = max(max(max(x) for x in board), result)
+```
+2048 게임의 규칙에 맞게 `move(dir)` 함수를 정의해 주어야 하는데, 전달받은 `dir` 값에 따라 `board` 배열을 순회하며 방향에 맞게 현재 조사하고 있는 줄에 대하여 알맞은 값을 `line`에 추가해 주면 된다.  
+`line`은 이동 방향에 따라 새로운 데이터를 추가해야 하는 방향이 달라지므로, 자료구조로 `deque`를 사용한다. 
+   
+각 칸을 순서에 맞게 순회하며 그 값이 `0`이라면 skip하고, `0`이 아닌 경우에는 같은 숫자가 연속으로 등장했을 때 합쳐지는 기능을 구현하기 위해 버퍼로 사용할 변수 하나를 두어 활용한다.  
+`0`이 아닌 숫자는 우선적으로 버퍼에만 저장해 두었다가 다음에 등장하는 `0`이 아닌 숫자가 현재 버퍼와
+1. 같다면 `현재 버퍼의 값 << 1` 을 `line`에 추가하고 버퍼를 비운다.
+2. 다르다면 현재 버퍼의 값을 `line`에 추가하고 버퍼에 현재 조사하는 값을 저장한다.
+반복문 종료 후 버퍼에 값이 저장되어 있다면 `line`에 추가해 주고, 저장된 `line`의 길이와 `N`값을 비교하여 부족한 길이만큼 `0`을 삽입해 준다.  
+  
+위와 같은 과정을 거치면 `line`에 현재 조사중인 줄의 이동 완료 후 데이터가 저장될 것이다. 이동방향을 고려하여 원본 배열의 해당 줄을 `line`으로 대체해 주면 된다.
 
 ## 🙂 마무리
-
+앞, 뒤에서 모두 삽입/삭제 연산이 일어날 경우 `deque`의 사용을 고려하자.  
+단순한 구조의 2차원 배열을 깊은 복사해야 하는 경우 `copy.deepcopy()` 보다 `[row[:] for row in 2DList]` 가 훨씬 빠르다. (상세 동작에는 물론 차이가 있음)  
+  
+자세한 설명은 아래 링크 참조  
+[Is python deepcopy more efficient... Stackoverflow](https://stackoverflow.com/questions/20772885/is-python-deepcopy-more-efficient-than-custom-code-or-less-efficient-predictabl)  
+[[Python] 파이썬 리스트 복사](https://codesyun.tistory.com/198)
