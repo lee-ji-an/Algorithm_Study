7fa083ed3d2e853fcd099070f97cd9fd157a7d71
HyungUk Ryu <hw0603@naver.com>
2023-02-05 18:29:26 +0900

    Create 11048 이동하기

---
+++ BOJ/DP/11048/hw0603/11048.py
@@ -0,0 +1,19 @@
+import sys
+
+d = ((-1, 0), (0, -1), (-1, -1))
+N, M = map(int, sys.stdin.readline().split())
+candy = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0]*M for _ in range(N)]
+
+for i in range(N):
+    for j in range(M):
+        maxval = 0
+        for dr, dc in d:
+            nr, nc = i+dr, j+dc
+            if not (0 <= nr < N and 0 <= nc < M):
+                continue
+            maxval = max(maxval, dp[nr][nc])
+        
+        dp[i][j] = candy[i][j] + maxval
+
+print(dp[-1][-1])
