bf0c1db3a61ef53dd96f23882ba1bc9be7a14adf
dntmdxor99 <dntmdxor99@naver.com>
2023-02-07 20:36:40 +0900

    Modify 10942 팰린드롬?

--- BOJ/DP/10942/dntmdxor99/10942.py
+++ BOJ/DP/10942/dntmdxor99/10942.py
@@ -11,8 +11,10 @@ def sol():
         dp[i][i] = 1
     for i in range(n - 1):
         dp[i][i + 1] = 1 if board[i] == board[i + 1] else 0     # i부터 i + 1번째까지
+    for i in range(n - 2):
+        dp[i][i + 2] = 1 if board[i] == board[i + 2] else 0
 
-    for j in range(2, n):      # i로부터 2칸 떨어진 애들부터 봄
+    for j in range(3, n):      # i로부터 2칸 떨어진 애들부터 봄
         for i in range(n - j):       # i부터 i + j까지 팰린드롬인지 봄, 2칸 떨어진 애들을 본다면 0~2, 1~3, 2~4가 최대 범위임
             if board[i] == board[i + j] and dp[i + 1][i + j - 1]:       # i와 i + j번째가 같고, i + 1 ~ i + j - 1이 팰린드롬이라면 i ~ i + j도 팰린드롬임
                 dp[i][i + j] = 1
