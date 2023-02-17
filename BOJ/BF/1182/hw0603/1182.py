28acd7ec5670a5b1f07336830a46c477aae9487f
HyungUk Ryu <hw0603@naver.com>
2023-02-17 13:42:55 +0900

    Modify 1182 부분수열의 합

--- BOJ/BF/1182/hw0603/1182.py
+++ BOJ/BF/1182/hw0603/1182.py
@@ -4,10 +4,4 @@ import sys
 N, S = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 
-cnt = 0
-for i in range(1, N+1):
-    for c in combinations(nums, i):
-        if (sum(c) == S):
-            cnt += 1
-
-print(cnt)
+print(len([True for i in range(1, N+1) for c in combinations(nums, i) if sum(c) == S]))
