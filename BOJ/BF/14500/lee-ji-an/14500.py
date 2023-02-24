fc583c6018aeebe1886cbded6c1d243819921f28
lee-ji-an <leeja0407@naver.com>
2023-02-24 20:50:15 +0900

    Modify 17822 원판 돌리기

--- BOJ/BF/14500/lee-ji-an/14500.py
+++ BOJ/BF/14500/lee-ji-an/14500.py
@@ -25,7 +25,7 @@ def dfs(y, x, value, cnt):
         return
     for i in range(4):
         if cnt == 1 and i == 0:
-                continue
+            continue
         movey = y + dy[i]
         movex = x + dx[i]
         if not (0 <= movey < N) or not(0 <= movex < M) or visited[movey][movex]:
@@ -33,26 +33,14 @@ def dfs(y, x, value, cnt):
         visited[movey][movex] = True
         if cnt == 2:
             dfs(y, x, value + board[movey][movex], cnt + 1)
-        # print("loc : ",movey, movex, "value, cnt : ", value + board[movey][movex], cnt + 1)
         dfs(movey, movex, value + board[movey][movex], cnt + 1)
         visited[movey][movex] = False
-
-
-def extra(y, x):
-    global max_value
-    for item in (-1, 1):
-        if 1 <= x <= M - 2 and 0 <= y + item < N:
-            max_value = max(max_value, board[y][x - 1] + board[y][x] + board[y][x + 1] + board[y + item][x])
-    for item in (-1, 1):
-        if 1 <= y <= N - 2 and 0 <= x + item < M:
-            max_value = max(max_value, board[y - 1][x] + board[y][x] + board[y + 1][x] + board[y][x + item])
 
 
 for i in range(N):
     for j in range(M):
         visited[i][j] = True
         dfs(i, j, board[i][j], 1)
-        # extra(i, j)
         visited[i][j] = False
-# dfs(0, 0, board[0][0], 1)
+
 print(max_value)
--- BOJ/Simulation/17822/lee-ji-an/17822.py
+++ BOJ/Simulation/17822/lee-ji-an/17822.py
@@ -16,7 +16,7 @@ for _ in range(T):
 
 for i in range(T):
     xi, di, ki = rotation[i]
-    if di == 0:
+    if di == 0:                             # 원판 회전
         for dnum in range(xi, N + 1, xi):
             for _ in range(ki):
                 disk[dnum].appendleft(disk[dnum].pop())
@@ -24,14 +24,17 @@ for i in range(T):
         for dnum in range(xi, N + 1, xi):
             for _ in range(ki):
                 disk[dnum].append(disk[dnum].popleft())
-    # print("rotate ", disk)
+
     flag = False
     del_list = set()
     for j in range(1, N + 1):
-        for k in range(j % 2, M):
+        range_list = [n for n in range(j % 2, M, 2)]  # 한 칸씩 띄워서 인접한 숫자를 검사
+        if M % 2 == 1 and j % 2 == 1:  # M 이 홀수이고 j가 홀수이면 0을 검사할 숫자에 추가
+            range_list.append(0)
+        for k in range_list:
             if disk[j][k] == 0:
                 continue
-            if disk[j][k] == disk[j][(k + 1) % M]:
+            if disk[j][k] == disk[j][(k + 1) % M]:   # 인접한 숫자 비교 -> 바꿔야 할 부분을 set에 집어넣음
                 del_list.add((j, k))
                 del_list.add((j, (k + 1) % M))
                 flag = True
@@ -47,9 +50,9 @@ for i in range(T):
                 del_list.add((j, k))
                 del_list.add((j + 1, k))
                 flag = True
-    for item in del_list:
+    for item in del_list:   # set에 있는 자리를 0으로 갱신
         disk[item[0]][item[1]] = 0
-    if not flag:
+    if not flag:  # 바뀐 게 없을 때 숫자 갱신
         cnt = 0
         total = 0
         for j in range(1, N + 1):
@@ -68,10 +71,8 @@ for i in range(T):
                     disk[j][k] -= 1
                 elif disk[j][k] < avg:
                     disk[j][k] += 1
-    # print(disk)
-# print(5 % M)
+
 total = 0
 for i in range(1, N + 1):
     total += sum(disk[i])
 print(total)
-
