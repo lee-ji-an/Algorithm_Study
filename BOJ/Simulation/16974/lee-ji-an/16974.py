0ed0463c04e336f0b84024f6581185b9b1c87a02
lee-ji-an <leeja0407@naver.com>
2023-02-26 21:12:24 +0900

    Create 16974 레벨 햄버거

---
+++ BOJ/Simulation/16974/lee-ji-an/16974.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N, X = map(int, input().split())
+
+
+def recursion(lv, x):
+    if x == 0:
+        return 0
+    if x == total[lv]:
+        return patty[lv]
+
+    if x <= total[lv - 1] + 2:  # x 가 중간에 패티가 들어가는 지점보다 같거나 왼쪽일 때
+        if x == total[lv - 1] + 2:
+            return recursion(lv - 1, x - 2) + 1  # x 가 중간에 패티가 들어가는 지점까지일 때
+        else:
+            return recursion(lv - 1, x - 1)  # # x 가 중간에 패티가 들어가는 지점보다 왼쪽일 때
+    else:
+        return patty[lv - 1] + 1 + recursion(lv - 1, x - total[lv - 1] - 2)  # # x 가 중간에 패티가 들어가는 지점보다 오른쪽일 때
+
+
+total = [1] * (N + 1)
+patty = [1] * (N + 1)
+for i in range(1, N + 1):
+    total[i] = total[i - 1] * 2 + 3  # level i 의 전체 햄버거 장 수 (패티 + 번)
+for i in range(1, N + 1):
+    patty[i] = patty[i - 1] * 2 + 1  # level i 의 전체 패티 장 수
+print(recursion(N, X))
