f9e186e2e81a692b7b5bd87d762d365849b8a8e7
HyungUk Ryu <hw0603@naver.com>
2023-03-13 23:22:03 +0900

    Modify 2374 같은 수로 만들기

--- BOJ/Divide_and_Conquer/2374/hw0603/2374.py
+++ BOJ/Divide_and_Conquer/2374/hw0603/2374.py
@@ -1,18 +1,17 @@
 import sys
 
-n = int(input())
+n = int(sys.stdin.readline())
 arr = [int(sys.stdin.readline()) for _ in range(n)]
-maxVal = prev = arr.pop(0)
+maxVal = max(arr)
+prev = arr.pop(0)
 cnt = 0
 
 for num in arr:
-    if (num == prev):
-        continue
-
+    # if (num == prev):
+    #     continue
     # 지금 보고 있는 숫자가 prev보다 큰 경우 그 차이만큼 답에 누적해 줌
     if (num > prev):
         cnt += (num - prev)
-        maxVal = max(maxVal, num) 
     prev = num
 
 print(cnt + maxVal - prev)
