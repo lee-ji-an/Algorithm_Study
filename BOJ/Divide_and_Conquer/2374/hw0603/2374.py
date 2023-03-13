f6fd58107cea5c8dd2bcc3c447b7b217802e1ace
HyungUk Ryu <hw0603@naver.com>
2023-03-14 00:33:25 +0900

    Modify 2374 같은 수로 만들기

--- BOJ/Divide_and_Conquer/2374/hw0603/2374.py
+++ BOJ/Divide_and_Conquer/2374/hw0603/2374.py
@@ -7,8 +7,8 @@ prev = arr.pop(0)
 cnt = 0
 
 for num in arr:
-    # if (num == prev):
-    #     continue
+    if (num == prev):
+        continue
     # 지금 보고 있는 숫자가 prev보다 큰 경우 그 차이만큼 답에 누적해 줌
     if (num > prev):
         cnt += (num - prev)
