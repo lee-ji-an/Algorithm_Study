a1e071f5591fd0cae91f73a296e116c1bcb109dc
lee-ji-an <leeja0407@naver.com>
2023-03-24 21:49:07 +0900

    Modify 1507 궁금한 민호

--- BOJ/Shortest_Path/1507/lee-ji-an/1507.py
+++ BOJ/Shortest_Path/1507/lee-ji-an/1507.py
@@ -10,35 +10,35 @@ res_adj = [[]]
 ans = 0
 
 
-for i in range(N):
-    row = list(map(int, input().split()))
-    res_adj.append([0] + row)
-    for j in range(i + 1, N):
-        heapq.heappush(hq, (row[j], i + 1, j + 1))
+def solve():
+    ans = 0
+    for i in range(N):
+        row = list(map(int, input().split()))
+        res_adj.append([0] + row)
+        for j in range(i + 1, N):
+            heapq.heappush(hq, (row[j], i + 1, j + 1))
 
-for i in range(N + 1):  # 나 자신으로 향하는 루트의 거리는 0
-    sp_list[i][i] = 0
+    for i in range(N + 1):  # 나 자신으로 향하는 루트의 거리는 0
+        sp_list[i][i] = 0
 
-flag = False
-while len(hq) != 0:
-    weight, v, w = heapq.heappop(hq)
-    if sp_list[v][w] == weight:
-        continue
+    while len(hq) != 0:
+        weight, v, w = heapq.heappop(hq)
+        if sp_list[v][w] == weight:
+            continue
 
-    for node in range(1, N + 1):
-        for node2 in range(1, N + 1):
-            if sp_list[node][v] >= 0 and sp_list[w][node2] >= 0:
-                distance = sp_list[node][v] + weight + sp_list[w][node2]
-                if res_adj[node][node2] > distance:  # 정답 길이보다 더 작은 길이가 나오면 flag 체크 -> -1 출력
-                    flag = True
-                    break
-                elif res_adj[node][node2] == distance:
-                    sp_list[node][node2] = distance
-                    sp_list[node2][node] = distance
-    if flag:
-        ans = -1
-        break
+        for node in range(1, N + 1):
+            for node2 in range(1, N + 1):
+                if sp_list[node][v] >= 0 and sp_list[w][node2] >= 0:
+                    distance = sp_list[node][v] + weight + sp_list[w][node2]
+                    if res_adj[node][node2] > distance:  # 정답 길이보다 더 작은 길이가 나오면 flag 체크 -> -1 출력
+                        return -1
+                    elif res_adj[node][node2] == distance:
+                        sp_list[node][node2] = distance
+                        sp_list[node2][node] = distance
 
-    ans += weight
+        ans += weight
 
-print(ans)
+    return ans
+
+
+print(solve())
