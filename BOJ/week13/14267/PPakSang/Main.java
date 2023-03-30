a9f03ea69dd1dbcfa4559da1cdac05aab7eca55f
PPakSang <sanghyun-dev@naver.com>
2023-03-30 19:43:36 +0900

    Chore: 14267 회사문화

--- BOJ/week13/14267/PPakSang/Main.java
+++ BOJ/week13/14267/PPakSang/Main.java
@@ -1,162 +1,73 @@
-package org.example.study.tree.가장가까운공통조상;
+package org.example.study.tree.회사문화;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
- * 2
- * 16
- * 1 14
- * 8 5
- * 10 16
- * 5 9
- * 4 6
- * 8 4
- * 4 10
- * 1 13
- * 6 15
- * 10 11
- * 6 7
- * 10 2
- * 16 3
- * 8 1
- * 16 12
- * 16 7
- * 5
- * 2 3
+ * 5 3
+ * -1 1 2 3 4
+ * 2 2
  * 3 4
- * 3 1
- * 1 5
- * 3 5
+ * 5 6
  */
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            int N = Integer.parseInt(br.readLine());
+        String[] temp = br.readLine().split(" ");
 
-            List<Integer>[] conn = new List[N+1];
-            boolean[] hasParent = new boolean[N+1];
-            orient = new Node[N+1];
-            for (int j = 0; j <= N; j++) {
-                conn[j] = new ArrayList<>();
-            }
+        int n = Integer.parseInt(temp[0]);
+        int m = Integer.parseInt(temp[1]);
 
-            for (int j = 0; j < N-1; j++) {
-                String[] temp = br.readLine().split(" ");
-                int n1 = Integer.parseInt(temp[0]);
-                int n2 = Integer.parseInt(temp[1]);
+        temp = br.readLine().split(" ");
 
-                hasParent[n2] = true;
-                conn[n1].add(n2);
-            }
+        List<Integer>[] hierarchy = new List[n+1];
+        int[] compliment = new int[n+1];
 
-            int root = -1;
-            for (int j = 1; j <= N; j++) {
-                if (!hasParent[j]) {
-                    root = j;
-                    break;
-                }
-            }
+        for (int i = 0; i <= n; i++) {
+            hierarchy[i] = new ArrayList<>();
+        }
 
-            String[] temp = br.readLine().split(" ");
-            int n1 = Integer.parseInt(temp[0]);
-            int n2 = Integer.parseInt(temp[1]);
+        for (int i = 1; i <= n; i++) {
+            int high = Integer.parseInt(temp[i-1]);
 
-            if (n1 == root || n2 == root) {
-                System.out.println(root);
+            if (high == -1) {
                 continue;
             }
 
-            if (n1 == n2) {
-                System.out.println(n1);
-                continue;
-            }
+            hierarchy[high].add(i);
+        }
 
-            dfs(root, conn, 1);
+        for (int i = 0; i < m; i++) {
+            temp = br.readLine().split(" ");
+            int to = Integer.parseInt(temp[0]);
+            int w = Integer.parseInt(temp[1]);
 
+            compliment[to] += w;
+        }
 
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
 
-            Node n1Orients = orient[n1];
-            Node n2Orients = orient[n2];
+        while(q.size() > 0) {
+            int president = q.poll();
 
-            int ii = n1Orients.depth;
-            int jj = n2Orients.depth;
-
-            while (ii >= 1 && jj >= 1) {
-                int n1Orient = n1Orients.parent;
-                int n2Orient = n2Orients.parent;
-
-                if (n1Orient == n2Orient) {
-                    System.out.println(n1Orient);
-                    break;
-                }
-
-                if (n1Orient == n2) {
-                    System.out.println(n2);
-                    break;
-                }
-
-                if (n2Orient == n1) {
-                    System.out.println(n1);
-                    break;
-                }
-
-                if (ii > jj) {
-                    n1Orients = orient[n1Orients.parent];
-                    ii--;
-                    continue;
-                }
-                n2Orients = orient[n2Orients.parent];
-                jj--;
+            for (int sub : hierarchy[president]) {
+                compliment[sub] += compliment[president];
+                q.add(sub);
             }
         }
-    }
 
-    static Node[] orient;
-    static void dfs(int parent, List<Integer>[] conn, int depth) {
-        for (int child : conn[parent]) {
-            orient[child] = new Node(parent, depth);
-            dfs(child, conn, depth+1);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(compliment[i]);
+            sb.append(" ");
         }
-    }
-
-    static class Node {
-        int parent;
-        int depth;
-
-        public Node(int parent, int depth) {
-            this.parent = parent;
-            this.depth = depth;
-        }
+        System.out.println(sb);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
