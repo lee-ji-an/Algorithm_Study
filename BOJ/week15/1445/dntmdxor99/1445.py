a944073fcfd3eb7568f06607fc3505a7cc40eb61
dntmdxor99 <dntmdxor99@naver.com>
2023-04-12 00:52:08 +0900

    Modify 1445 일요일 아침의 데이트

--- BOJ/week15/1445/dntmdxor99/1445.py
+++ BOJ/week15/1445/dntmdxor99/1445.py
@@ -3,30 +3,51 @@ from collections import deque
 input = sys.stdin.readline
 
 
+def sol(flag):
+    # 다음 위치의 값과 현재 위치를 비교함
+    i, j = ((0, 0), (1, 0), (0, 0), (0, 1))[flag]       # 현재 위치에 쓰레기, 인접한 곳에 쓰레기
+
+    if ct[ny][nx] < ct[y][x] + i:
+        return False  # 이전 + i이 가려는 곳보다 크면 갈 필요 없음
+
+    elif ct[ny][nx] == ct[y][x] + i:  # 만약 같다면?
+        if cat[ny][nx] <= cat[y][x] + j: return False  # 가려는 곳보다 이전이 같거나 크면 갈 필요 없음
+
+    ct[ny][nx] = ct[y][x] + i
+    cat[ny][nx] = cat[y][x] + j
+
+    if flag:
+        dq.append([ny, nx])
+
+    return True
+
+
 if __name__ == "__main__":
     n, m = map(int, input().split())
     maps = [list(input().strip()) for _ in range(n)]
     ct = [[2500] * m for _ in range(n)]        # check trash
     cat = [[2500] * m for _ in range(n)]       # check adjacent trash
-    adj = [[0] * m for _ in range(n)]
     dir = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
     dq = deque()
 
     for y in range(n):
         for x in range(m):
-            if maps[y][x] == 'S':
-                dq.append([y, x])
-                ct[y][x] = cat[y][x] = 0
-            elif maps[y][x] == 'F':
-                out_y, out_x = y, x
-            elif maps[y][x] == '.':
-                for dy, dx in dir:
-                    ny, nx = y + dy, x + dx
-                    if 0 <= ny < n and 0 <= nx < m:
-                        if maps[ny][nx] == 'g':     # 쓰레기 근처를 지날때 adj를 1로 변경함
-                            adj[y][x] = 1
-                            break
+            match maps[y][x]:
+                case 'S':
+                    dq.append([y, x])
+                    ct[y][x] = cat[y][x] = 0
+
+                case 'F':
+                    out_y, out_x = y, x
+
+                case '.':
+                    for dy, dx in dir:
+                        ny, nx = y + dy, x + dx
+                        if 0 <= ny < n and 0 <= nx < m:
+                            if maps[ny][nx] == 'g':     # 쓰레기 근처를 지날때 maps[y][x]를 a로 변경함
+                                maps[y][x] = 'a'
+                                break
 
     while dq:
         y, x = dq.popleft()
@@ -34,30 +55,18 @@ if __name__ == "__main__":
         for dy, dx in dir:
             ny, nx = y + dy, x + dx
             if 0 <= ny < n and 0 <= nx < m:
-                if maps[ny][nx] == 'g':     # 쓰레기를 밟을 때
-                    if ct[ny][nx] < ct[y][x] + 1: continue      # 이전 + 1이 가려는 곳보다 크면 갈 필요 없음
-                    elif ct[ny][nx] == ct[y][x]:        # 만약 같다면?
-                        if cat[ny][nx] <= cat[y][x]: continue       # 가려는 곳보다 이전이 같거나 크면 갈 필요 없음
+                match maps[ny][nx]:
+                    case 'g':     # 쓰레기를 밟을 때
+                        if not sol(1): continue
 
-                    ct[ny][nx] = ct[y][x] + 1
-                    cat[ny][nx] = cat[y][x]
-                    dq.append([ny, nx])
+                    case '.':       # 땅을 밟을 때
+                        if not sol(2): continue
 
-                elif maps[ny][nx] == '.':       # 땅을 밟을 때
-                    if ct[ny][nx] < ct[y][x]: continue      # 이전이 가려는 곳보다 쓰레기 값이 크다면 갈 필요 없음
-                    if ct[ny][nx] == ct[y][x]:
-                        if cat[ny][nx] <= cat[y][x] + adj[ny][nx]: continue     # 가려는 곳보다 이전의 인접한 쓰레기 개수가 크거나 같다면 갈 필요 없음
+                    case 'a':
+                        if not sol(3): continue
 
-                    ct[ny][nx] = ct[y][x]
-                    cat[ny][nx] = cat[y][x] + adj[ny][nx]
-                    dq.append([ny, nx])
+                    case 'F':
+                        if not sol(0): continue
 
-                elif maps[ny][nx] == 'F':
-                    if ct[ny][nx] < ct[y][x]: continue
-                    elif ct[ny][nx] == ct[y][x]:
-                        cat[ny][nx] = min(cat[ny][nx], cat[y][x])
-                    elif ct[ny][nx] > ct[y][x]:
-                        ct[ny][nx] = ct[y][x]
-                        cat[ny][nx] = cat[y][x]
 
     print(ct[out_y][out_x], cat[out_y][out_x])
