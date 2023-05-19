d59df3ad8d788e97b7dba89e900b19197ad6b078
dntmdxor99 <dntmdxor99@naver.com>
2023-05-19 18:02:28 +0900

    Modify 1484 다이어트

--- BOJ/week20/1484/dntmdxor99/1484.py
+++ BOJ/week20/1484/dntmdxor99/1484.py
@@ -1,20 +1,18 @@
-if __name__ == "__main__":
-    n = int(input())
-
-    maps = [i**2 for i in range(1, n)]      # (n-1)^2 - (n-2)^2 > n if n > 2, 따라서 n - 1까지 제곱수를 구해도 상관 없음
+def two_pointer():
+    maps = [i ** 2 for i in range(1, n)]  # (n-1)^2 - (n-2)^2 > n if n > 2, 따라서 n - 1까지 제곱수를 구해도 상관 없음
     # 1, 2는 아래의 while 문의 n - 1에서 처리됨
 
     left = 0
     right = 1
     ans = []
 
-    while left < right < n - 1:     # right가 left보다 항상 크도록
+    while left < right < n - 1:  # right가 left보다 항상 크도록
         val = maps[right] - maps[left]
-        if val > n:     # val이 더 크다면 왼쪽 포인터를 옮겨야 함
+        if val > n:  # val이 더 크다면 왼쪽 포인터를 옮겨야 함
             left = min(left, n - 2) + 1
-        elif val < n:       # val이 더 작다면 오른쪽 포인터를 옮겨야 함
+        elif val < n:  # val이 더 작다면 오른쪽 포인터를 옮겨야 함
             right = min(right, n - 2) + 1
-        else:       # 같다면 후보지에 추가, 양쪽 다 옮겨도 상관 없음
+        else:  # 같다면 후보지에 추가, 양쪽 다 옮겨도 상관 없음
             ans.append(right + 1)
             left += 1
             right += 1
@@ -23,4 +21,27 @@ if __name__ == "__main__":
         print(*ans, sep='\n')
     else:
         print(-1)
+
+
+def math():
+    from math import sqrt
+    ans = []
+    for i in range(1, n + 1):
+        val = i**2 - n
+        if val > 0:
+            if sqrt(i**2 - n).is_integer():
+                ans.append(i)
+
+    if ans:
+        print(*ans, sep='\n')
+    else:
+        print(-1)
+
+
+if __name__ == "__main__":
+    n = int(input())
+
+    math()
+    # two_pointer()
+
 
