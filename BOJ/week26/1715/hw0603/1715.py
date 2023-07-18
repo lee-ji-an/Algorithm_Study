979db8bda3021584b18195db3f4caccffe434687
HyungUk Ryu <hw0603@naver.com>
2023-07-18 15:33:09 +0900

    Create 1715 카드 정렬하기

---
+++ BOJ/week26/1715/hw0603/1715.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+N = int(sys.stdin.readline())
+cardlist = [int(sys.stdin.readline()) for _ in range(N)]
+heapq.heapify(cardlist)
+
+result = 0
+while (len(cardlist) > 1):
+    # 현재 덱들 중 가장 작은 두 덱 비교
+    compare = heapq.heappop(cardlist)+heapq.heappop(cardlist)
+    result += compare
+    heapq.heappush(cardlist, compare)  # 비교 후 생성된 덱 큐에 추가
+
+print(result)
