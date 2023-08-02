b5a71bd41c4030380019e096117cdaa4c805974f
HyungUk Ryu <hw0603@naver.com>
2023-08-03 04:17:56 +0900

    Create 1300 K번째 수

---
+++ BOJ/week28/1300/hw0603/1300.py
@@ -0,0 +1,21 @@
+import sys
+
+N = int(sys.stdin.readline())
+K = int(sys.stdin.readline())
+
+start, end = 1, K
+while (start <= end):
+    mid = (start + end) // 2  # 중간값 찌름
+
+    # 각 행에서 mid 이하 원소의 개수
+    cnt = sum(
+        min(mid//i, N) for i in range(1, N+1)
+    )
+    
+    if (cnt >= K):
+        ans = mid
+        end = mid - 1  # 탐색범위 왼쪽으로
+    else:
+        start = mid + 1  # 탐색범위 오른쪽으로
+
+print(ans)
