64d18dac2f8a71bd133e5a63ef84ec872138bf34
HyungUk Ryu <hw0603@naver.com>
2023-08-03 04:22:57 +0900

    Modify 1300 K번째 수

--- BOJ/week28/1300/hw0603/1300.py
+++ BOJ/week28/1300/hw0603/1300.py
@@ -3,7 +3,7 @@ import sys
 N = int(sys.stdin.readline())
 K = int(sys.stdin.readline())
 
-start, end = 1, K
+start, end = 1, K*K
 while (start <= end):
     mid = (start + end) // 2  # 중간값 찌름
 
@@ -12,6 +12,7 @@ while (start <= end):
         min(mid//i, N) for i in range(1, N+1)
     )
     
+    # mid 이하 원소의 개수가 K개 이상이면
     if (cnt >= K):
         ans = mid
         end = mid - 1  # 탐색범위 왼쪽으로
