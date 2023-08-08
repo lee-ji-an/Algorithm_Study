3c518029f69b1feb22449e6f0a4216ca7be21c63
lee-ji-an <leeja0407@naver.com>
2023-08-08 17:34:46 +0900

    Modify 1300 K번째 수

--- BOJ/week28/1300/lee-ji-an/1300.py
+++ BOJ/week28/1300/lee-ji-an/1300.py
@@ -9,15 +9,12 @@ start, end = 1, N * N
 while start <= end:
     mid = (start + end) // 2
 
-    # min_cnt = mid 보다 작은 숫자의 갯수
     # max_cnt = mid 보다 작거나 같은 숫자의 갯수
-    min_cnt = sum(min(mid // i, N) if mid % i > 0 else min((mid // i) - 1, N) for i in range(1, N + 1))
     max_cnt = sum(min(mid // i, N) for i in range(1, N + 1))
 
     # 정답 찾으면 종료
-    if min_cnt < order <= max_cnt:
+    if order <= max_cnt:
         ans = mid
-        break
 
     if order > max_cnt:
         start = mid + 1
