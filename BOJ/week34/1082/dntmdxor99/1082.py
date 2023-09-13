9f5d6b409ee69b6e8b3ebb61437e0485a392d2f8
dntmdxor99 <dntmdxor99@naver.com>
2023-09-14 01:15:59 +0900

    Create 1082 방 번호

---
+++ BOJ/week34/1082/dntmdxor99/1082.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+if __name__ == "__main__":
+    n = int(input())
+    costs = list(map(int, input().split()))
+    m = int(input())
+    dp = [-float('inf') for _ in range(m + 1)]
+
+    for y in range(n - 1, -1, -1):
+        cost = costs[y]     # 값을 받아옴
+        for x in range(cost, m + 1):        # 해당 방 번호의 값 이후로만 살 수 있음
+            dp[x] = max(y * (x // cost), dp[x], dp[x - cost] * 10 + y)      # 높은 숫자로 일단 꽉 채운 값, 기존 값, 한 개만 넣었을 때의 값
+
+    print(dp[m])
