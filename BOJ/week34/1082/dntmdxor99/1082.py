01e8461e768b6892341c045e86f2c7da29235146
dntmdxor99 <dntmdxor99@naver.com>
2023-09-14 12:46:50 +0900

    Modify 1028 방 번호

--- BOJ/week34/1082/dntmdxor99/1082.py
+++ BOJ/week34/1082/dntmdxor99/1082.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     for y in range(n - 1, -1, -1):
         cost = costs[y]     # 값을 받아옴
         for x in range(cost, m + 1):        # 해당 방 번호의 값 이후로만 살 수 있음
-            dp[x] = max(y * (x // cost), dp[x], dp[x - cost] * 10 + y)      # 높은 숫자로 일단 꽉 채운 값, 기존 값, 한 개만 넣었을 때의 값
+            dp[x] = max(y * int('1' * (x // cost)), dp[x], dp[x - cost] * 10 + y)      # 높은 숫자로 일단 꽉 채운 값, 기존 값, 한 개만 넣었을 때의 값
 
     print(dp[m])
