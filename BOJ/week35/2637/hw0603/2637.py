6d7ab83a6badf2bbff33b98aac13bc3a2dc09952
HyungUk Ryu <hw0603@naver.com>
2023-09-17 17:55:13 +0900

    Create 2637 장난감 조립

--- BOJ/week35/2637/hw0603/2637.py
+++ BOJ/week35/2637/hw0603/2637.py
@@ -1,65 +1,52 @@
 from collections import defaultdict, deque
 import sys
 
+
 N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 
 data = defaultdict(list)
-refCnt = defaultdict(set)
-deps = defaultdict(set)
+deps = defaultdict(set)  # k 를 만들기 위해 필요한 부품
+referenced = defaultdict(set)  # k 가 있어야 만들 수 있는 부품
 
 for _ in range(M):
     X, Y, K = map(int, sys.stdin.readline().strip().split())
-    data[X].append((Y, K))
-    refCnt[X].add(Y)
-    deps[Y].add(X)
+    data[X].append((Y, K))  # X를 만들기 위해 Y가 K개 필요
+    deps[X].add(Y)  # X를 만들기 위해 Y가 필요
+    referenced[Y].add(X)  # Y가 있어야 X를 만들 수 있음
 
-# 기본 부품과 중간 부품 구분
+# 기본 부품과 중간 부품들의 번호 구분
 basic = set()
 middle = set()
 for k, val in data.items():
     for item in val:
         (basic if item[0] not in data.keys() else middle).add(item[0])
 
-# print(basic, middle)
+q = deque()
+for k in deps:
+    deps[k] -= basic  # 차집합 연산
+    if not (deps[k]):
+        q.append(k)  # 기본 부품들만으로 만들 수 있는 중간 부품들(의존성 없는 부품들)을 모두 큐에 삽입
 
 result = {}
-q = deque()
-for k in refCnt:
-    refCnt[k] -= basic  # 차집합 연산
-    if (len(refCnt[k]) == 0):
-        q.append(k)  # 기본 부품들만으로 만들 수 있는 중간 부품들을 다 큐에 삽입
-
-for p in basic:
-    del deps[p]
-
 while (q):
     partIdx = q.popleft()
-    result[partIdx] = {}
+    result[partIdx] = defaultdict(int)
     for y, k in data[partIdx]:
+        # 중간 부품이라면 그것을 구성하는 부품들의 카운트를 각각 증가
         if (y in middle):
             for a, b in result[y].items():
-                if (result[partIdx].get(a)):
-                    result[partIdx][a] += b*k
-                else:
-                    result[partIdx][a] = b*k
+                result[partIdx][a] += b*k
+        # 기본 부품이라면 자신의 카운트를 증가
         else:
-            if (result[partIdx].get(y)):
-                result[partIdx][y] += k
-            else:
-                result[partIdx][y] = k
+            result[partIdx][y] += k
     
     # 나 때문에 의존성 걸렸던 애들 해제
-    for p in deps[partIdx]:
-        refCnt[p] -= {partIdx}
-        if (len(refCnt[p]) == 0):
-            q.append(p)
+    for p in referenced[partIdx]:
+        deps[p] -= {partIdx}  # 차집합 연산
+        if not (deps[p]):
+            q.append(p)  # 의존성이 모두 해제된 부품은 큐에 추가
 
-
-# print(refCnt)
-# print(data)
-# print(deps)
-# print(result)
 
 for k in sorted(result[N]):
     print(k, result[N][k])
